768d48fcb5f5a8f05394556e4fa8e1d9c9b6cffa
Rick South <rick@natamus.com>
2024-03-09 13:52:09 +0100

    (v4.5) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/respawndelay/util/Reference.java
+++ Common/src/main/java/com/natamus/respawndelay/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.respawndelay.util;
 public class Reference {
 	public static final String MOD_ID = "respawndelay";
 	public static final String NAME = "Respawn Delay";
-	public static final String VERSION = "4.4";
+	public static final String VERSION = "4.5";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
