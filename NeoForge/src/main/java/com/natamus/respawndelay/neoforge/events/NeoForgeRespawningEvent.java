38c12c87a9e70ff871d5d595fd1c9286c37b483a
Rick South <rick@natamus.com>
2024-04-28 01:27:11 +0200

    Respawn Delay source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/respawndelay/neoforge/events/NeoForgeRespawningEvent.java
@@ -0,0 +1,64 @@
+package com.natamus.respawndelay.neoforge.events;
+
+import com.natamus.respawndelay.cmds.CommandRespawnall;
+import com.natamus.respawndelay.events.RespawningEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeRespawningEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandRespawnall.register(e.getDispatcher());
+	}
+
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || e.phase != Phase.START) {
+			return;
+		}
+
+		RespawningEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+
+	@SubscribeEvent
+	public static void onPlayerDeath(LivingDeathEvent e) {
+		Entity entity = e.getEntity();
+		Level level = entity.level();
+		if (level.isClientSide) {
+			return;
+		}
+
+		if (!(entity instanceof Player)) {
+			return;
+		}
+
+		if (!RespawningEvent.onPlayerDeath((ServerPlayer)entity, e.getSource(), 0)) {
+			e.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlayerLogout(PlayerEvent.PlayerLoggedOutEvent e) {
+		Player player = e.getEntity();
+		RespawningEvent.onPlayerLogout(player.level(), player);
+	}
+
+	@SubscribeEvent
+	public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent e) {
+		Player player = e.getEntity();
+		RespawningEvent.onPlayerLogin(player.level(), player);
+	}
+}
