88daabc3b36bdc35e4d56d77ff56344a946b85e1
Rick South <rick@natamus.com>
2024-06-14 05:14:01 +0200

    Respawn Delay source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/respawndelay/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.respawndelay.util;
+
+public class Reference {
+	public static final String MOD_ID = "respawndelay";
+	public static final String NAME = "Respawn Delay";
+	public static final String VERSION = "4.6";
+	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
+}
