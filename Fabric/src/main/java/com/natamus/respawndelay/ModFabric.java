eec46fba7887fae875da2312d089313ed7c3115d
Rick South <rick@natamus.com>
2024-07-28 07:16:51 +0200

    (v4.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/respawndelay/ModFabric.java
+++ Fabric/src/main/java/com/natamus/respawndelay/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.respawndelay;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
 import com.natamus.respawndelay.cmds.CommandRespawnall;
 import com.natamus.respawndelay.events.RespawningEvent;
@@ -19,6 +20,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- Forge/src/main/java/com/natamus/respawndelay/ModForge.java
+++ Forge/src/main/java/com/natamus/respawndelay/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.respawndelay;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.respawndelay.forge.config.IntegrateForgeConfig;
 import com.natamus.respawndelay.forge.events.ForgeRespawningEvent;
 import com.natamus.respawndelay.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
