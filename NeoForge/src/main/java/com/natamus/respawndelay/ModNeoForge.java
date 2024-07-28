1f0b2d0fa68897ba0e3a21d0c14264f53ae20c7f
Rick South <rick@natamus.com>
2024-07-28 07:16:58 +0200

    (v4.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/respawndelay/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/respawndelay/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.respawndelay;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.respawndelay.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.respawndelay.neoforge.events.NeoForgeRespawningEvent;
 import com.natamus.respawndelay.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
