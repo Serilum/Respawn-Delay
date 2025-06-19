6c2b0e781d64f441083a2da06b371066d8986f79
Rick South <rick@natamus.com>
2025-06-19 03:38:20 +0200

    Respawn Delay source code for 1.21.6.

---
+++ Common/src/main/java/com/natamus/respawndelay/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.respawndelay;
+
+import com.natamus.respawndelay.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
